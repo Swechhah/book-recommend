ad8ad96fa77e70e79d6e54e5392b707c6e7e0858
Swechha Humagain <swechhahumagain@nchl.local>
2025-11-26 10:32:20 +0545

    Initial commit

---
+++ recommendor/src/main/java/com/platform/recommendor/RecommendorApplication.java
@@ -0,0 +1,13 @@
+package com.platform.recommendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendorApplication.class, args);
+    }
+
+}
---
+++ recommendor/src/test/java/com/platform/recommendor/RecommendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.recommendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
