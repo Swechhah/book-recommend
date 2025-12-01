944ef1c62f7f8678f90d2adfbbe5786c730b6260
Swechha Humagain <swechhahumagain@nchl.local>
2025-12-01 17:59:35 +0545

    Docker configured

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="swechhahumagain"
+
+ENTRYPOINT ["top", "-b"]
--- recommendor/src/main/java/com/platform/recommendor/app/presentation/BookImportController.java
+++ recommendor/src/main/java/com/platform/recommendor/app/presentation/BookImportController.java
@@ -2,7 +2,6 @@ package com.platform.recommendor.app.presentation;
 
 import com.platform.recommendor.app.application.dto.user.LoginResponse;
 import com.platform.recommendor.app.application.usecases.BookDataCSVImporter;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
